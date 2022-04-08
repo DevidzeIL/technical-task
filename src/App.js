262de8349da5f1636fdd3bca9f3c717030bf2655
DevidzeIL <devidzeil@icloud.com>
2022-04-08 04:34:14 +0400

    added AWS Cognito service

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./App.css"
 
+
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 const API_HOST = "https://dw7oi55gga.execute-api.us-east-1.amazonaws.com/";
 const API_URL = "v1/getcurrentweather/";
 
@@ -11,7 +13,9 @@ function App() {
   const inputChangeHandler = (event) => setCity(event.target.value);
 
   const btnClickHandler = () =>
-    fetch(API_HOST + API_URL + `?city=${city}`)
+    fetch(API_HOST + API_URL + `?city=${city}`, {
+      headers: { 'Authorization': ACCESS_TOKEN }
+  })
       .then((response) => response.json())
       .then((data) =>
         setWeather(`Temperature: ${data.temperature} C°
